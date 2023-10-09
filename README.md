59786c2979d83c9f186790a5e02a1e5f13d8fd67
shahedsuleiman <shahedsuleiman2001@gmail.com>
2023-10-09 23:21:11 +0300

    api get method

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fakestoreapi
+## i fetched data from a fake api and created an object for each product thin pushed it to an array , then i display every product and its info in a card in the main section.
---
+++ app.js
@@ -0,0 +1,41 @@
+class Product{
+    constructor (title, price, description , image){
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+    }
+}
+
+
+const products = [];
+
+fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            const productData = data[i];
+            const product = new Product(
+                productData.title,
+                productData.price,
+                productData.description,
+                productData.image
+            );
+            products.push(product);
+        }
+        const mainSection = document.getElementById('main-section');
+
+        products.map(product => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = `
+                <img  style="width:250px;height:200px; " src="${product.image}" alt="${product.title}">
+                <h2>${product.title}</h2>
+                <p>Price: ${product.price}</p>
+                <p> ${product.description}</p>
+            `;
+            mainSection.appendChild(card);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
